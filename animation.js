207f61bc2b24d2d313057590efbe6b64546b95fd
jenyu7 <jyu7@stuy.edu>
2018-02-10 14:35:21 -0500

    added dvd animation

--- animation.js
+++ animation.js
@@ -2,7 +2,7 @@
  * Jen Yu 
  * SoftDev Period 7
  * 02-07-2018
- * K#02: They lock us in the tower whenever we get caught...which is often
+ * K #03: What is it saving the screen from?
  */
 
 var canvas = document.getElementById("slate");
@@ -11,9 +11,11 @@ var width = canvas.getAttribute("width");
 var ctx = canvas.getContext("2d"); // set context to 2d
 
 var animate = document.getElementById("animate");
+var dvd_a = document.getElementById("dvd");
 var stop = document.getElementById("stop");
 
 console.log(animate);
+console.log(dvd_a);
 console.log(stop);
 
 //initialized radius
@@ -31,6 +33,7 @@ var update = function(r){
 
 //draws a dot and increases/decreases radius
 var drawDot = function(){
+    stopA();
     ctx.fillStyle = "#ff0000";
     ctx.beginPath();
     ctx.clearRect(0, 0, 600, 600);
@@ -45,6 +48,30 @@ var drawDot = function(){
     id = window.requestAnimationFrame(drawDot);
 };
 
+var dvd = new Image();
+dvd.src = "dvd.png";
+
+var dvdBounce = function(){
+    stopA();
+    //initializing
+    var x = Math.random() * (canvas.width - dvd.width);
+    var y = Math.random() * (canvas.height - dvd.height);
+    var dx = 3 * Math.random() + 0.1;
+    var dy = 3 * Math.random() + 0.1;
+    var bounce = function(){
+	ctx.clearRect(0, 0, 600, 600);
+	if (x < 0 || x > canvas.width - dvd.width)
+	    dx *= -1;
+	if (y < 0 || y > canvas.height - dvd.height)
+	    dy *= -1;
+	ctx.drawImage(dvd, x+dx, y+dy);
+	x += dx;
+	y += dy;
+	id = window.requestAnimationFrame(bounce);
+    };
+    bounce();
+}
+
 //stops the animation (id known through global var)
 var stopA = function(){
     window.cancelAnimationFrame(id);
@@ -52,4 +79,5 @@ var stopA = function(){
 
 //add the event listeners
 animate.addEventListener("click", drawDot);
+dvd_a.addEventListener("click", dvdBounce);
 stop.addEventListener("click", stopA);
